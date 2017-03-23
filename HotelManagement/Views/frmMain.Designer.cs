45379bcea610aed87f68cefabf074221b6400452
hoadinh216 <dinhthihoa.mta@gmail.com>
2017-03-23 21:02:16 +0700

    Cap nhat giao dien chung

--- HotelManagement/Views/frmMain.Designer.cs
+++ HotelManagement/Views/frmMain.Designer.cs
@@ -59,7 +59,7 @@
             this.panel1.Controls.Add(this.btnTrangChu);
             this.panel1.Location = new System.Drawing.Point(2, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(233, 705);
+            this.panel1.Size = new System.Drawing.Size(188, 705);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -67,7 +67,7 @@
             this.pictureBox1.Image = global::HotelManagement.Properties.Resources._12512603_467712036748896_4824590879508992717_n;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(233, 165);
+            this.pictureBox1.Size = new System.Drawing.Size(188, 165);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -76,7 +76,7 @@
             this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnThoat.Location = new System.Drawing.Point(0, 431);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(233, 52);
+            this.btnThoat.Size = new System.Drawing.Size(188, 52);
             this.btnThoat.TabIndex = 5;
             this.btnThoat.Text = "THOÁT";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -87,7 +87,7 @@
             this.btnThongKe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnThongKe.Location = new System.Drawing.Point(0, 379);
             this.btnThongKe.Name = "btnThongKe";
-            this.btnThongKe.Size = new System.Drawing.Size(233, 52);
+            this.btnThongKe.Size = new System.Drawing.Size(188, 52);
             this.btnThongKe.TabIndex = 4;
             this.btnThongKe.Text = "THỐNG KÊ";
             this.btnThongKe.UseVisualStyleBackColor = true;
@@ -98,7 +98,7 @@
             this.btnTinhTrangPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnTinhTrangPhong.Location = new System.Drawing.Point(0, 327);
             this.btnTinhTrangPhong.Name = "btnTinhTrangPhong";
-            this.btnTinhTrangPhong.Size = new System.Drawing.Size(233, 52);
+            this.btnTinhTrangPhong.Size = new System.Drawing.Size(188, 52);
             this.btnTinhTrangPhong.TabIndex = 3;
             this.btnTinhTrangPhong.Text = "TÌNH TRẠNG PHÒNG";
             this.btnTinhTrangPhong.UseVisualStyleBackColor = true;
@@ -109,7 +109,7 @@
             this.btnQLKhach.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnQLKhach.Location = new System.Drawing.Point(0, 223);
             this.btnQLKhach.Name = "btnQLKhach";
-            this.btnQLKhach.Size = new System.Drawing.Size(233, 52);
+            this.btnQLKhach.Size = new System.Drawing.Size(188, 52);
             this.btnQLKhach.TabIndex = 1;
             this.btnQLKhach.Text = "QUẢN LÝ KHÁCH";
             this.btnQLKhach.UseVisualStyleBackColor = true;
@@ -120,7 +120,7 @@
             this.btnDatPhongNhanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnDatPhongNhanh.Location = new System.Drawing.Point(0, 275);
             this.btnDatPhongNhanh.Name = "btnDatPhongNhanh";
-            this.btnDatPhongNhanh.Size = new System.Drawing.Size(233, 52);
+            this.btnDatPhongNhanh.Size = new System.Drawing.Size(188, 52);
             this.btnDatPhongNhanh.TabIndex = 2;
             this.btnDatPhongNhanh.Text = "ĐẶT PHÒNG NHANH";
             this.btnDatPhongNhanh.UseVisualStyleBackColor = true;
@@ -131,7 +131,7 @@
             this.btnTrangChu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.btnTrangChu.Location = new System.Drawing.Point(0, 171);
             this.btnTrangChu.Name = "btnTrangChu";
-            this.btnTrangChu.Size = new System.Drawing.Size(233, 52);
+            this.btnTrangChu.Size = new System.Drawing.Size(188, 52);
             this.btnTrangChu.TabIndex = 0;
             this.btnTrangChu.Text = "TRANG CHỦ";
             this.btnTrangChu.UseVisualStyleBackColor = true;
@@ -144,10 +144,10 @@
             this.tabControlMain.Controls.Add(this.tabPageDatPhongNhanh);
             this.tabControlMain.Controls.Add(this.tabPageTinhTrangPhong);
             this.tabControlMain.Controls.Add(this.tabPageThongKe);
-            this.tabControlMain.Location = new System.Drawing.Point(241, 4);
+            this.tabControlMain.Location = new System.Drawing.Point(190, 1);
             this.tabControlMain.Name = "tabControlMain";
             this.tabControlMain.SelectedIndex = 0;
-            this.tabControlMain.Size = new System.Drawing.Size(943, 610);
+            this.tabControlMain.Size = new System.Drawing.Size(994, 613);
             this.tabControlMain.TabIndex = 0;
             // 
             // tabPageTrangChu
@@ -155,7 +155,7 @@
             this.tabPageTrangChu.Location = new System.Drawing.Point(4, 24);
             this.tabPageTrangChu.Name = "tabPageTrangChu";
             this.tabPageTrangChu.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageTrangChu.Size = new System.Drawing.Size(935, 582);
+            this.tabPageTrangChu.Size = new System.Drawing.Size(986, 585);
             this.tabPageTrangChu.TabIndex = 1;
             this.tabPageTrangChu.Text = "Trang chủ";
             this.tabPageTrangChu.UseVisualStyleBackColor = true;
@@ -164,7 +164,7 @@
             // 
             this.tabPageQLKhach.Location = new System.Drawing.Point(4, 24);
             this.tabPageQLKhach.Name = "tabPageQLKhach";
-            this.tabPageQLKhach.Size = new System.Drawing.Size(935, 582);
+            this.tabPageQLKhach.Size = new System.Drawing.Size(986, 585);
             this.tabPageQLKhach.TabIndex = 2;
             this.tabPageQLKhach.Text = "Quản Lý Khách";
             this.tabPageQLKhach.UseVisualStyleBackColor = true;
@@ -173,7 +173,7 @@
             // 
             this.tabPageDatPhongNhanh.Location = new System.Drawing.Point(4, 24);
             this.tabPageDatPhongNhanh.Name = "tabPageDatPhongNhanh";
-            this.tabPageDatPhongNhanh.Size = new System.Drawing.Size(935, 582);
+            this.tabPageDatPhongNhanh.Size = new System.Drawing.Size(986, 585);
             this.tabPageDatPhongNhanh.TabIndex = 3;
             this.tabPageDatPhongNhanh.Text = "Đặt Phòng Nhanh";
             this.tabPageDatPhongNhanh.UseVisualStyleBackColor = true;
@@ -182,7 +182,7 @@
             // 
             this.tabPageTinhTrangPhong.Location = new System.Drawing.Point(4, 24);
             this.tabPageTinhTrangPhong.Name = "tabPageTinhTrangPhong";
-            this.tabPageTinhTrangPhong.Size = new System.Drawing.Size(935, 582);
+            this.tabPageTinhTrangPhong.Size = new System.Drawing.Size(986, 585);
             this.tabPageTinhTrangPhong.TabIndex = 4;
             this.tabPageTinhTrangPhong.Text = "Tình trạng phòng";
             this.tabPageTinhTrangPhong.UseVisualStyleBackColor = true;
@@ -191,7 +191,7 @@
             // 
             this.tabPageThongKe.Location = new System.Drawing.Point(4, 24);
             this.tabPageThongKe.Name = "tabPageThongKe";
-            this.tabPageThongKe.Size = new System.Drawing.Size(935, 582);
+            this.tabPageThongKe.Size = new System.Drawing.Size(986, 585);
             this.tabPageThongKe.TabIndex = 5;
             this.tabPageThongKe.Text = "Thống kê";
             this.tabPageThongKe.UseVisualStyleBackColor = true;
@@ -208,7 +208,7 @@
             this.MinimumSize = new System.Drawing.Size(1200, 650);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmMain";
+            this.Text = "Hoarom Hotel";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.panel1.ResumeLayout(false);
